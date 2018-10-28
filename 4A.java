f1a031f2bc6ecc307e4e67a8a4b870ac9b595609
raj-humble <35659984+raj-humble@users.noreply.github.com>
2018-10-28 17:22:25 +0530

    Update 4A.java

--- 4A.java
+++ 4A.java
@@ -2,16 +2,19 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-
-public class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+public class Codechef {
+	
+	public static void main (String[] args) throws java.lang.Exception {
+		
 		Scanner scan = new Scanner(System.in);
+		
 		int w = scan.nextInt();
+		
 		if(w ==2 || w % 2 ==1)
-		System.out.println("NO");
+			System.out.println("NO");
 		else if(w % 2 == 0)
-		System.out.println("YES");
+			System.out.println("YES");
+		
 	}
+	
 }
